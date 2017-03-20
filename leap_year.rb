be9b2d7fb9f7d995c34fd4c01d479f8aa780d65d
rwilhel <rwilhel@gmail.com>
2017-03-20 10:34:40 -0600

    "Edits to leap_year.rb"

--- leap_year.rb
+++ leap_year.rb
@@ -1,11 +1,14 @@
 puts "Enter a year! "
 year = gets.to_i
-
-  if (year%4 == 0) && (year%100 != 0) && (year%400 == 0)
+case
+  when (year%4 == 0) 
+       (year%100 != 0)
+       (year%400 == 0)
     puts "You got yourself a leap year!"
-  elsif
+  else
     puts "This isn't a leap year. *sad trombone*"
-  end
+end
+
 
 
 
